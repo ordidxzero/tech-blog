7f2b1010ef3f2643edef770fdf05178c44b1e1d7
kim-macbook <creator@hanyang.ac.kr>
2020-12-24 19:38:35 +0900

    image 파일들의 경로를 수정하고 Netlify CMS를 설치함

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,14 +17,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/images`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `logos`,
-        path: `${__dirname}/src/images/logos`,
+        path: `${__dirname}/static/images/logos`,
       },
     },
     {
@@ -42,7 +42,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -56,6 +56,9 @@ module.exports = {
       options: {
         dest: `./src/@types/graphql-types.d.ts`,
       },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
     },
     {
       resolve: `gatsby-plugin-postcss`,

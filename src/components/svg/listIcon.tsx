6839148d5119a1d3453028c0a2d075218fb542ec
YuJun Heo <creator@hanyang.ac.kr>
2023-07-18 00:09:29 +0900

    Update listIcon.tsx

--- src/components/svg/listIcon.tsx
+++ src/components/svg/listIcon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ListIcon = () => {
   return (
     <div className="w-4 h-4 cursor-pointer">
-      <img src="https://www.flaticon.com/svg/static/icons/svg/456/456243.svg" />
+      <img src="https://cdn-icons-png.flaticon.com/512/151/151917.png" />
     </div>
   );
 };

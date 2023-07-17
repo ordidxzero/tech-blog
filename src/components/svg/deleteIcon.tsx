9e6864d6f1f054c3b4864ec469d8efb40c7e878e
YuJun Heo <creator@hanyang.ac.kr>
2023-07-18 00:00:58 +0900

    Update deleteIcon.tsx

--- src/components/svg/deleteIcon.tsx
+++ src/components/svg/deleteIcon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DeleteIcon = (props: React.DOMAttributes<HTMLDivElement>) => {
   return (
     <div className="w-4 h-4 cursor-pointer" {...props}>
-      <img src="https://www.flaticon.com/svg/static/icons/svg/1828/1828843.svg" />
+      <img src="https://cdn-icons-png.flaticon.com/512/1828/1828843.png" />
     </div>
   );
 };
